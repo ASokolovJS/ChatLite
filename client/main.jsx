813e34de0b68691c2ff015ffd0bab05ca274bd6c
Aleksandr <76512284+ASokolovJS@users.noreply.github.com>
2021-12-14 15:47:27 +0300

    Update main.jsx

--- client/main.jsx
+++ client/main.jsx
@@ -3,6 +3,3 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import '../imports/api/router'
 import ChatMess from "../imports/Collections/ChatMess";
 import Chat from "../imports/Collections/Сhat";
-
-window.Mess = new ChatMess()
-window.Chat = new Chat()
