6be4f9ce0b504a43e8b48befb001d5eb9d5e5eea
ASokolovJS <AYsokolov94@yandex.ru>
2021-12-14 13:51:02 +0300

    Chage server

--- server/main.js
+++ server/main.js
@@ -2,19 +2,4 @@ import {Meteor} from "meteor/meteor";
 import {check} from "meteor/check";
 import {Accounts} from "meteor/accounts-base";
 
-Meteor.methods({
-  Registration(name, email, password){
-      check(name, String)
-      check(email, String)
-      check(password, String)
 
-      if(!Accounts.findUserByEmail(email)){
-        Accounts.createUser({
-          username: name,
-          email: email,
-          password: password
-        })
-        console.log("User is created")
-      }
-    }
-})
